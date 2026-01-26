db18a3d15f5f8763966a2a99a7394b07a0938ddd
deterraleon <ivolohov95@gmail.com>
2026-01-26 08:21:52 +0500

    added function calling, recursion updates

--- compile.py
+++ compile.py
@@ -6,9 +6,11 @@ def process(func)->str:
     load_dotenv()
     sp = os.getenv("SP")
     sp1 = os.getenv("SP1")
+    sp2 = os.getenv("SP2")
     func = str(func)
     func = func.replace('"', sp)
     func = func.replace("'", sp1)
+    func = func.replace("\n", sp2)
     return func
 
 def unprocess(func):
@@ -91,6 +93,7 @@ def process_function_calls(text:list[str], names):
         if line.count("[functions[i][0], *process_function_calls(functions[i], names)"):
             pass
         for name in names:
+            indent = get_indent(line)
             if line.count(name) > 0:
                 inputs = [""]
                 write = 0
@@ -110,15 +113,33 @@ def process_function_calls(text:list[str], names):
                 reqs = functoreq[name]
                 if len(inputs) != len(reqs):
                     raise Exception(f"{len(inputs)} variables recieved, but expected {len(reqs)}")
-                indent = get_indent(line)
                 for i in range(len(inputs)):
                     newtext.append(f"{indent}save_variable(get_variable('{inputs[i]}'), '{reqs[i]}', 1)\n")
+                newtext.append(indent + "save_variable(get_variable(os.getenv('RECURSION'))+1, os.getenv('RECURSION'))\n")
                 start = line.find(name)
-                line = line[:start] + f"exec({name[:-1]})" + line[start + line[start:].find(")")+1:]
+                newtext.append(indent + """cur.execute(f'SELECT program FROM {os.getenv("PROGRAM_NAME")}) WHERE name = """ + f"\"{name[:-1]}\"" + "')\n")
+                line = line[:start] + "exec(unprocess(cur.fetchone()))" + line[start + line[start:].find(")")+1:]
+        if line.count("return"):
+            newtext.append(indent + "save_variable(get_variable(os.getenv('RECURSION'))-1, os.getenv('RECURSION'))\n")
         newtext.append(line)
     return newtext
         
-
+def save_function(text:list[str], name):
+    load_dotenv()
+    global con, cur
+    program = os.getenv("PROGRAM_NAME")
+    variables = os.getenv("VARIABLES_NAME")
+    newtext = ""
+    beg = 0
+    if text[0].count("def") > 0:
+        beg = 1
+    for line in text[beg:]:
+        newtext += line
+    newtext = process(newtext)
+    print(f"INSERT INTO {program} (program, name) VALUES ('{newtext}', '{name[:-1]}')")
+    cur.execute(f"INSERT INTO {program} (program, name) VALUES ('{newtext}', '{name[:-1]}')")
+    con.commit()
+    
         
 
 
@@ -129,7 +150,9 @@ cur = con.cursor()
 program = os.getenv("PROGRAM_NAME")
 variables = os.getenv("VARIABLES_NAME")
 cur.execute(f"CREATE TABLE IF NOT EXISTS {variables} (name str, value TEXT)")
-cur.execute(f"CREATE TABLE IF NOT EXISTS {program} (name, value TEXT)")
+cur.execute(f"CREATE TABLE IF NOT EXISTS {program} (name str, program TEXT)")
+cur.execute(f"DELETE FROM {program}")
+cur.execute(f"DELETE FROM {variables}")
 with open("input.py", "r") as input:
     text = input.readlines()
     functions, main = parce_functions(text)
@@ -140,6 +163,9 @@ with open("input.py", "r") as input:
     for i in range(len(functions)):
         functions[i] = [functions[i][0], *process_function_calls(functions[i], names)]
     main = process_function_calls(main, names)
+    for i in range(len(functions)):
+        save_function(functions[i], names[i])
+    save_function(main, "main(")
     with open("output.py", "w") as output:
         for func in functions:
             for line in func:
@@ -147,4 +173,5 @@ with open("input.py", "r") as input:
             output.write("\n\n")
         for line in main:
             output.write(line)
+
     
